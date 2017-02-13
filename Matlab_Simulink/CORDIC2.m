bcbbfe5455bdbc571b2cf7cfaf2e32719a2305d7
cdumonde <cedric.dumondelle1@gmail.com>
2017-02-13 10:33:26 +0100

    Bug fixe

---
+++ Matlab_Simulink/CORDIC2.m
@@ -0,0 +1,17 @@
+clc;
+clear;
+close all;
+epsilon = [0.54931 0.25541 0.12566 0.06258 0.03126 0.01563 0.00781 0.00391 0.00391 0.00195 0.00097 0.00049 0.00024 0.00012 6E-5];
+x = 1.20513;
+y = 0;
+theta = input('angle ? ');
+z = theta;
+for i = 1 : 14
+    sgn = (z > 0)*2 - 1;
+    temp = x + sgn*y/(2^i);
+    y = y + sgn*x/(2^i);
+    x = temp;
+    z = z-sgn*epsilon(i);
+end
+x + y
+exp(theta)
--- Matlab_Simulink/test_cordic.m
+++ Matlab_Simulink/test_cordic.m
@@ -4,16 +4,17 @@ clear;
 close all;
 
 m = -1;
-theta = 0:0.01:pi/2;
-n = input('Combien d iteration ? ');
+theta = -1:0.01:1;
+n = input('Combien d iterations ? ');
 k = 1 : n+1;
 K = 0.80;
 epsilon = atanh(2.^-k);
 for i = 1:length(theta)
-    x0 = 1.20513;
+    x0 = prod(cosh(epsilon));
     y0 = 0;
     [X(i), Y(i)] = CORDIC(m, epsilon, x0, y0, theta(i), n);
 end
-plot(theta, Y*K);
-hold on;
-plot(theta, sinh(theta));
+figure;
+plot(theta, (X+Y), theta, exp(theta));
+figure;
+plot(theta, abs((X+Y) - exp(theta))./exp(theta)*100);
