5d6bfd487677d1bbd40ef6ec286df00ca54ea131
lestandin <lestandi.nathan@gmail.com>
2017-02-27 12:10:05 +0100

    Ambroise method

--- Matlab_Simulink/CORDIC_M_A.m
+++ Matlab_Simulink/CORDIC_M_A.m
@@ -2,3 +2,18 @@ clc
 clear
 close all
 
+tau = 2^(-17);
+IS(1)=1;
+
+for i = 1:1:655360
+IS(i+1)= IS(i)-tau*IS(i);
+end
+figure,
+x_axis= 0:1:655360;
+exptau= exp(-x_axis.*tau);
+plot(x_axis, IS, 'b')
+hold on
+plot(x_axis,exptau,'-r')
+
+figure,
+plot(x_axis,(IS-exptau)./exptau)
